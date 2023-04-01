45e05bb8263ad5d0fd5e11d733323184ec460b2b
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-04-01 10:37:31 +0300

    doing 1 and 2 hm

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
 
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
+
+const { start: startButton, stop: stopButton } = refs;
+
+startButton.addEventListener('click', onStartClick);
+
+stopButton.addEventListener('click', onStopClick);
+
+let intervalId = null
+
+function onStartClick() {
+    document.body.style.backgroundColor = getRandomHexColor();
+
+    startButton.setAttribute('disabled', 'disabled');
+
+    intervalId = setInterval(
+      () => (document.body.style.backgroundColor = getRandomHexColor()),
+      1000
+    );
+}
+
+function onStopClick() {
+    clearInterval(intervalId);
+    startButton.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,82 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  daysTxt: document.querySelector('span[data-days]'),
+  hoursTxt: document.querySelector('span[data-hours]'),
+  minutesTxt: document.querySelector('span[data-minutes]'),
+  secondsTxt: document.querySelector('span[data-seconds]'),
+};
+
+const { btnStart, daysTxt, hoursTxt, minutesTxt, secondsTxt } = refs;
+
+let intervalId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() - Date.now() < 0) {
+      Notiflix.Notify.warning('Please choose a date in the future', {
+        rtl: true,
+      });
+    } else {
+      btnStart.addEventListener('click', onBthStartClick);
+
+      function onBthStartClick() {
+        btnStart.setAttribute('disabled', 'disabled')
+
+        let timeLeft = selectedDates[0].getTime() - Date.now();
+        let { days, hours, minutes, seconds } = convertMs(timeLeft);
+
+        daysTxt.textContent = addLeadingZero(String(days));
+        hoursTxt.textContent = addLeadingZero(String(hours));
+        minutesTxt.textContent = addLeadingZero(String(minutes));
+        secondsTxt.textContent = addLeadingZero(String(seconds));
+
+        intervalId = setInterval(() => {
+          timeLeft = selectedDates[0].getTime() - Date.now();
+          let { days, hours, minutes, seconds } = convertMs(timeLeft);
+
+          daysTxt.textContent = addLeadingZero(String(days));
+          hoursTxt.textContent = addLeadingZero(String(hours));
+          minutesTxt.textContent = addLeadingZero(String(minutes));
+          secondsTxt.textContent = addLeadingZero(String(seconds));
+          console.log(selectedDates[0].getTime() - Date.now());
+          if (!(selectedDates[0].getTime() - Date.now())) {
+            clearInterval(intervalId);
+          }
+        }, 1000);
+      }
+    }
+  },
+};
+
+flatpickr('input#datetime-picker', options);
+
+function addLeadingZero(value) {
+  return value.padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
